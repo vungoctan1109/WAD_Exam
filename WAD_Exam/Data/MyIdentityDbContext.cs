3531cb5698261894cafc5620c1d600586fd528f7
vungoctan1109 <vntan1109@gmail.com>
2022-03-17 13:33:19 +0700

    first commit

---
+++ WAD_Exam/Data/MyIdentityDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WAD_Exam.Data
+{
+    public class MyIdentityDbContext
+    {
+    }
+}
---
+++ WAD_Exam/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WAD_Exam.MvcApplication" Language="C#" %>
